0fa075fb6efcc4b975ea806a72382bc86b056249
Arpit Blagan <blaganarpit@gmail.com>
2024-11-04 17:07:26 +0530

    writing tests first

--- apps/tests/index.test.ts
+++ apps/tests/index.test.ts
@@ -27,19 +27,79 @@ describe("authentication", () => {
   });
 });
 
+async function waitForIncomingMessageAndPop(messagesArr: any[]) {
+  await new Promise((r) => {
+    if (messagesArr.length > 0) {
+      r(messagesArr.shift());
+    } else {
+      let interval = setInterval(() => {
+        if (messagesArr.length > 0) {
+          r(messagesArr.shift());
+          clearInterval(interval);
+        }
+      }, 100);
+    }
+  });
+}
+
 describe("ws checkup", () => {
   let ws1: WebSocket;
   let ws2: WebSocket;
+  let messages1: any[];
+  let messages2: any[];
   beforeAll(async () => {
     //Simple create two ws connection to check its working properly
     ws1 = new WebSocket("");
+    ws1.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      messages1.push(message);
+    };
     await new Promise((r) => {
       ws1.onopen = r;
     });
     ws2 = new WebSocket("");
-
+    ws2.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      messages2.push(message);
+    };
     await new Promise((r) => {
       ws2.onopen = r;
     });
+  });
+  test("try joining an arena", async () => {
+    ws1.send(
+      JSON.stringify({
+        type: "join-arena",
+      })
+    );
+    const message1: any = await waitForIncomingMessageAndPop(messages1);
+    ws2.send(
+      JSON.stringify({
+        type: "join-arena",
+      })
+    );
+    const message2: any = await waitForIncomingMessageAndPop(messages2);
+    expect(message1.type).toBe("joined-arena");
+    expect(message2.type).toBe("joined-arena");
+  });
+
+  test("test movement events", async () => {
+    ws1.send(
+      JSON.stringify({
+        type: "movement",
+      })
+    );
+    const message: any = await waitForIncomingMessageAndPop(messages2);
+    expect(message.type).toBe("movement");
+  });
+  test("when some user leave the arena", async () => {
+    ws1.send(
+      JSON.stringify({
+        type: "",
+      })
+    );
+    const message2: any = await waitForIncomingMessageAndPop(messages2);
+
+    expect(message2.type).toBe("user-left");
   });
 });
